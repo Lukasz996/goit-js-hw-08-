70334add63514df16cc486f463a2e021549afc99
Łukasz <lukaszkogut1996@gmail.com>
2023-11-19 16:57:12 +0100

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const imagesHTML = galleryItems.map(({ preview, original, description }) => {
+	return `<div class='gallery__item'>
+	  <a class='gallery__link' href='${original}'>
+		<img class='gallery__image' src='${preview}' data-source='${original}' alt='${description}' />
+	  </a>
+	</div>`
+})
+gallery.insertAdjacentHTML('beforeend', imagesHTML.join(''))
+
+
+
+gallery.addEventListener('click', event => {
+	event.preventDefault()
+	if (event.target.tagName === 'IMG') {
+		const instance = basicLightbox.create(
+			`
+      <img src="${event.target.dataset.source}" alt="${event.target.alt}">`,
+			{
+				onClose: instance => {
+					gallery.removeEventListener('keydown', onEscapePress)
+				},
+			}
+		)
+
+		const onEscapePress = event => {
+			if (event.key === 'Escape') {
+				instance.close()
+			}
+		}
+		instance.show()
+		gallery.addEventListener('keydown', onEscapePress)
+	}
+})
